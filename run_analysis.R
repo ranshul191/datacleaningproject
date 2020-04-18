c1ce4e57bbd6ea6c298e4a5ddd54b3bb38bc8a6d
ranshul191 <rahuljha.jha@gmail.com>
2020-04-18 14:06:59 +0530

    completed the code

--- run_analysis.R
+++ run_analysis.R
@@ -31,9 +31,32 @@ names(dataTest)<-c(c("subject", "activity"), features)
 data<- rbind(dataTrain, dataTest)
 
 ##2. Extract only the measurements on the mean and standard deviation for each measurement
+##check for column names which have any form of mean or std in them
 dataExtracted<-data[,which(colnames(data) %in% c("subject", "activity", grep("mean()|std()", colnames(data), value=TRUE)))]
 ##look at the extracted data
 ##str(dataExtracted)
 
 ##3.Use descriptive activity names to name the activities in the data set
+##using the activity labels list we give descriptive activity names to the activities
 dataExtracted$activity <- activitylabels[dataExtracted$activity]
+
+##4.Appropriately labels the data set with descriptive variable names.
+# prefix t is replaced by time
+# Acc is replaced by Accelerometer
+# Gyro is replaced by Gyroscope
+# prefix f is replaced by frequency
+# Mag is replaced by Magnitude
+# BodyBody is replaced by Body
+names(dataExtracted)[-c(1:2)] <-gsub("^t","time", names(dataExtracted)[-c(1:2)])
+names(dataExtracted)[-c(1:2)] <-gsub("^f","frequency", names(dataExtracted)[-c(1:2)])
+names(dataExtracted)[-c(1:2)] <-gsub("Acc","Accelerometer", names(dataExtracted)[-c(1:2)])
+names(dataExtracted)[-c(1:2)] <-gsub("Gyro","Gyroscope", names(dataExtracted)[-c(1:2)])
+names(dataExtracted)[-c(1:2)] <-gsub("Mag","Magnitude", names(dataExtracted)[-c(1:2)])
+names(dataExtracted)[-c(1:2)] <-gsub("BodyBody","Body", names(dataExtracted)[-c(1:2)])
+
+##view names 
+##str(names(dataExtracted))
+
+##5.From the data set in step 4, create a second, independent tidy data set with the average of each variable for each activity and each subject
+## we first group by activity and subject, then summarize all the columns for their mean and sort and then convert it back to dataframe
+dataTidy <- dataExtracted %>%group_by(activity, subject)%>%summarize_all(mean)%>%arrange(activity, subject)%>%ungroup%>%as.data.frame()
